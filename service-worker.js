22bb6be6f624e83bbd408f8eb3fefb1c112c4d70
Kodi0508 <kodi0508rk@gmail.com>
2021-02-21 09:55:41 +0400

    Add Files

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+var cacheName = 'cw1-v1';
+var cacheFiles = [
+    'IndividualCoursework-1.html',
+    'products.js',
+    'cw1.webmanifest',
+    'French.jpg',
+    'Book.jpg',
+    'Spanish.jpg',
+    'Sports.jpg',
+    'Theatre.jpg',
+    'Geography.jpg',
+    'Music.jpg',
+    'Literature.jpg',
+    'History.jpg',
+    'Cooking.jpg',
+    'Wallpaper.png'
+];
+
+self.addEventListener('install', (e) => { 
+    console.log('[Service Worker] Install'); 
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => { 
+            console.log('[Service Worker] Caching all the files'); 
+            return cache.addAll(cacheFiles); 
+        })
+        ); 
+    });
