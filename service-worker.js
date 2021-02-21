660eecfb5e356a74b2be37b2ee5834784b08ac97
Kodi0508 <kodi0508rk@gmail.com>
2021-02-21 10:43:44 +0400

    service-worker.js updated

--- service-worker.js
+++ service-worker.js
@@ -26,13 +26,26 @@ self.addEventListener('install', (e) => {
         ); 
     });
 
-    self.addEventListener('fetch', function (e) {    
-        e.respondWith(
-        caches.match(e.request).then(function (r) {
-            console.log('[Service Worker] Fetching resource: '              
-            + e.request.url);
-            return r         
-        })    
-        );
-    }
-    );
+    // self.addEventListener('fetch', function (e) {    
+    //     e.respondWith(
+    //     caches.match(e.request).then(function (r) {
+    //         console.log('[Service Worker] Fetching resource: '              
+    //         + e.request.url);
+    //         return r         
+    //     })    
+    //     );
+    // }
+    // );
+
+    self.addEventListener('fetch', function (e) {  
+        e.respondWith(    
+            caches.match(e.request).then(function (r) {
+               return r || fetch(e.request).then(function (response) {
+                   return caches.open(cacheName).then(function (cache) {          
+                       cache.put(e.request, response.clone());
+                       return response;        
+                    });      
+                });    
+            })  
+            );
+        });
