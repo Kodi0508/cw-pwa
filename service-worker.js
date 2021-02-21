fd4416b9e0196206eae63d2b5acaf78808ceff6c
Kodi0508 <kodi0508rk@gmail.com>
2021-02-21 11:00:07 +0400

    Install updated

--- service-worker.js
+++ service-worker.js
@@ -16,15 +16,25 @@ var cacheFiles = [
     'Wallpaper.png'
 ];
 
-self.addEventListener('install', (e) => { 
-    console.log('[Service Worker] Install'); 
+self.addEventListener('install', (e) => {​​
+
+    console.log('[Service Worker] Install');
+
     e.waitUntil(
-        caches.open(cacheName).then((cache) => { 
-            console.log('[Service Worker] Caching all the files'); 
-            return cache.addAll(cacheFiles); 
-        })
-        ); 
-    });
+
+        caches.open(cacheName).then((cache) => {​​
+
+            console.log('[Service Worker] Caching all the files');
+
+            return cache.addAll(cacheFiles);
+
+        }​​)
+
+    );
+
+}​​);
+
+
 
     // self.addEventListener('fetch', function (e) {    
     //     e.respondWith(
